3ae8b3f0d1753469e5d21bc1a3fc4f6a783cfa6f
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-03-14 15:08:35 -0300

    Exercicio botao toggle com forEach

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-const BOTAO = document.querySelector('#calcular');
-BOTAO.addEventListener('click', () => alert("Fui clicado"));
+const botao = document.querySelector("[data-tipo=botao]");
+const lista = document.querySelectorAll("li.item");
+
+    botao.addEventListener('click', escondeLista)
+
+function escondeLista(){
+lista.forEach( (li) => {
+            li.classList.toggle('hidden')
+    })};
